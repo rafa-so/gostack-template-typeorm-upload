78111b709f651f3c45ab1a38383fcf2bbb17d454
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-07-28 00:43:21 -0300

    Create categories and transcations migratiions

---
+++ src/database/migrations/1595905577217-CreateTransactions.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransactions1595905577217
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'value',
+            type: 'numeric',
+            isNullable: true,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
