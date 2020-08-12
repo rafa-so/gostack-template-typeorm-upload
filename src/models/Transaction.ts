4d300a0eedf1f640b4af7a951ba76406c268c60a
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-08-11 22:22:02 -0300

    Ajustado a relação com os modelos transaction e category

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -3,7 +3,7 @@ import {
   Column,
   CreateDateColumn,
   PrimaryGeneratedColumn,
-  OneToOne,
+  ManyToOne,
   JoinColumn,
 } from 'typeorm';
 
@@ -26,7 +26,7 @@ class Transaction {
   @Column()
   category_id: string;
 
-  @OneToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
